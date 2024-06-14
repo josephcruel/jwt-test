054f4c224753556031460246bc42c1f67320e22b
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-06-14 14:03:55 +1200

    added User.js

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Sechema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongooes.model('User', UserSchema)
