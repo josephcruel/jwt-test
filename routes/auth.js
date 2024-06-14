442cb2af2aa596ad32a9751ee840b2135f1f8996
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-06-14 13:43:59 +1200

    setup auth.js

---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/User')
+
+const router = express.Router()
+
+router.post('/register', async (req, res) => {
+    const {name, email, password} = req.body
+
+    try {
+        let user = await User.findOne({email})
+
+        if(user) {
+            return res.status(400).json({ msg: ' User already exists'})
+        }
+
+        user = new User({ name, email, password})
+
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save()
+
+        const payload = {user: { id: user.id}}
+
+        jwt.sign(payload, 'your_jwt_secret', {expiresIn: 360000}, (err, token) => {
+            if(err) throw err
+            res.json({token})
+        })
+    } catch(err) {
+        res.status(500).send('Server Error')
+    }
+
+})
