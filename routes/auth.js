5eab4498cc7dadd4af7bf493091a4b08315b8562
Joseph Cruel <Joseph-Cruel@live.nmit.ac.nz>
2024-06-14 13:52:26 +1200

    part2 of auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -34,3 +34,33 @@ router.post('/register', async (req, res) => {
     }
 
 })
+
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body 
+
+    try {
+        let user = await User.findOne({email})
+         
+        if(!user){
+            return res.status(400).json({ msg: 'Invalid Credentials'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch){
+            return res.status(400).json({ msg: 'Invalid Credentials'})
+        }
+
+        const payload = { user: { id: user.id}}
+
+        jwt.sign(payload, 'your_jwt_secret', {expiresIn: 360000}, (err, token) => {
+            if(err) throw err
+            res.json({token})
+        })
+
+    } catch(err) {
+        res.status(500).send('Server error')
+    }
+})
+
+module.exports = router
